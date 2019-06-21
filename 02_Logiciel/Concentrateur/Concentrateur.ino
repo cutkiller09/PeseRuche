2e500c1e1107a6f345e85000cfe0e68acd40fb51
sebastien loisel <sebastien.loisel@free.fr>
2019-06-21 16:57:45 +0200

    Concentrateur et PeseRuche initialisé

---
+++ 02_Logiciel/Concentrateur/Concentrateur.ino
@@ -0,0 +1,122 @@
+#include <SPI.h>
+#include <mcp2515.h>
+
+#define DEBUG
+#define ID 3
+#define KEY_REINIT 0x55
+#define K_POS_ID_CAN_FRAME 7
+#define K_POS_REINIT_CAN_FRAME 6
+
+#ifdef DEBUG
+char buffer[8]; 
+#endif
+
+struct can_frame canMsg;
+MCP2515 mcp2515(10);
+
+
+void Init_Can() 
+{   
+  Serial.begin(115200);
+  SPI.begin();
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
+}
+
+
+void Envoi_Can(float f_Poids, char ID_CAN, char Reinit_Value) 
+{
+Serial.print("Envoi_Can:"); 
+#ifdef DEBUG
+  Serial.print("f_Poids:");  
+  Serial.println(f_Poids, 1);
+#endif
+  
+  canMsg.can_id  = 0x0F6;
+  canMsg.can_dlc = 8;
+  //memcpy(&canMsg.data[0],&Poids,8);  
+  canMsg.data[0]=(char)f_Poids;
+  canMsg.data[K_POS_ID_CAN_FRAME]=(char)ID_CAN;
+  canMsg.data[K_POS_REINIT_CAN_FRAME]=(char)Reinit_Value;
+ 
+
+#ifdef DEBUG
+  sprintf(buffer, "%d", (char)f_Poids); 
+  Serial.print("buffer:");  
+  Serial.print(buffer[0]);
+  Serial.print(buffer[1]);
+  Serial.print(buffer[2]);
+  Serial.print(buffer[3]);
+  Serial.print(buffer[4]);
+  Serial.print(buffer[5]);
+  
+  Serial.print("canMsg.data[0]:");  
+  Serial.println(canMsg.data[0], 1);
+  Serial.print("canMsg.data[1]:");  
+  Serial.println(canMsg.data[1], 1);
+  Serial.print("canMsg.data[2]:");  
+  Serial.println(canMsg.data[2], 1);
+  Serial.print("canMsg.data[3]:");  
+  Serial.println(canMsg.data[3], 1);
+  Serial.print("canMsg.data[4]:");  
+  Serial.println(canMsg.data[4], 1);
+  Serial.print("canMsg.data[5]:");  
+  Serial.println(canMsg.data[5], 1);
+  Serial.print("canMsg.data[6]:");  
+  Serial.println(canMsg.data[6], 1);
+  Serial.print("canMsg.data[7]:");  
+  Serial.println(canMsg.data[7], 1); 
+#endif 
+  
+  mcp2515.sendMessage(&canMsg); 
+
+  Serial.println("Messages sent");
+  
+  delay(100);
+
+}
+
+void Reinitalisation_Rucher(){
+  Envoi_Can(0,0,KEY_REINIT);
+  Serial.println("Reinitalisation_Rucher");
+}
+
+void Simulation_PeseRuche_Test(){  
+  Envoi_Can(0,ID,0);
+}
+
+void Lecture_Can()
+{ 
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+      
+Serial.print("Lecture_Can:"); 
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+        
+      //Serial.print(canMsg.data[i],HEX);
+      Serial.println(canMsg.data[i], 1);
+      //Serial.print(" ");
+
+    }
+
+    Serial.println();      
+  }
+}
+
+void setup() {
+  Init_Can();
+  Reinitalisation_Rucher();
+}
+
+void loop() {
+  Lecture_Can();
+}
