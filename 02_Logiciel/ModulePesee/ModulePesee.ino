1c291b5757a57e5ee821e27ff328be3b65e7a00b
sebastien loisel <sebastien.loisel@free.fr>
2019-06-21 17:16:43 +0200

    Mise au propre du module

--- 02_Logiciel/ModulePesee/ModulePesee.ino
+++ 02_Logiciel/ModulePesee/ModulePesee.ino
@@ -8,8 +8,7 @@
 #define DEBUG
 #define K_POS_ID_CAN_FRAME 7
 #define K_POS_REINIT_CAN_FRAME 6
- 
-
+  
 static boolean b_Reinit=0;
   
 /******************/
@@ -48,11 +47,13 @@ void Reinitialiser(unsigned int *pu8_ID_CAN){
 void Lecture_Can(unsigned int *pu8_ID_CAN)
 { 
   if (mcp2515.readMessage(&canMsg_Read) == MCP2515::ERROR_OK) {
+#ifdef DEBUG
     Serial.print("Lecture CAN");   
     Serial.print(canMsg_Read.can_id, HEX); // print ID
     Serial.print(" "); 
     Serial.print(canMsg_Read.can_dlc, HEX); // print DLC
     Serial.print(" ");
+#endif
     
     for (int i = 0; i<canMsg_Read.can_dlc; i++)  {  // print the data
         
@@ -88,13 +89,11 @@ void Lecture_Can(unsigned int *pu8_ID_CAN)
   }
 }
 
+/*************************************************************/
+/*            Module d'envoi de Trames CAN                   */
+/*************************************************************/
 void Envoi_Can(const unsigned int u8_Poids, const char ID_CAN) 
-{
-
-#ifdef DEBUG
-  Serial.print("u8_Poids:");  
-  Serial.println(u8_Poids, 1);
-#endif
+{ 
   
   canMsg_Write.can_id  = 0x0F6;
   canMsg_Write.can_dlc = 8;
@@ -104,7 +103,7 @@ void Envoi_Can(const unsigned int u8_Poids, const char ID_CAN)
  
 
 #ifdef DEBUG
-    Serial.print("Ecriture CAN");    
+  Serial.print("Ecriture CAN");    
   
   Serial.print("canMsg_Write.data[0]:");  
   Serial.println(canMsg_Write.data[0], 1); 
@@ -114,24 +113,24 @@ void Envoi_Can(const unsigned int u8_Poids, const char ID_CAN)
   Serial.println(canMsg_Write.data[K_POS_ID_CAN_FRAME], 1); 
 #endif 
   
-  mcp2515.sendMessage(&canMsg_Write); 
-
-  Serial.println("Messages sent");
+  mcp2515.sendMessage(&canMsg_Write);  
   
   delay(100);
 
 }
 
+/*************************************************************/
+/*            Module d'envoi d'Initialisaion du Poids        */
+/*************************************************************/
 void Init_Poids()
-{
-  Serial.println("HX711 Demo");
-
-  Serial.println("Initializing the scale");
+{ 
+ 
   // parameter "gain" is ommited; the default value 128 is used by the library
   // HX711.DOUT  - pin #A1
   // HX711.PD_SCK - pin #A0
   scale.begin(A1, A0);
 
+#ifdef DEBUG
   Serial.println("Before setting up the scale:");
   Serial.print("read: \t\t");
   Serial.println(scale.read());     // print a raw reading from the ADC
@@ -145,10 +144,12 @@ void Init_Poids()
   Serial.print("get units: \t\t");
   Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
             // by the SCALE parameter (not set yet)
+#endif
 
   scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();               // reset the scale to 0
 
+#ifdef DEBUG
   Serial.println("After setting up the scale:");
 
   Serial.print("read: \t\t");
@@ -165,23 +166,23 @@ void Init_Poids()
             // by the SCALE parameter set with set_scale
 
   Serial.println("Readings:");  
-} 
-
-void setup() {
-  Serial.begin(38400);
-  Init_Poids();
-  Init_Can(); 
-}
+#endif
+}  
 
 
+/*************************************************************/
+/*             Module de lecture du Poids                    */
+/*************************************************************/
 void Lecture_Poids(unsigned int *pu8_Poids,unsigned int *pu8_ID_CAN) {
   float f_Poids=0;
   
   f_Poids=scale.get_units(10);
+#ifdef DEBUG
   Serial.print("one reading:\t");
   Serial.print(scale.get_units(), 1);
   Serial.print("\t| average:\t");
   Serial.println(f_Poids, 1);
+#endif
   if (f_Poids>0)
   {
     *pu8_Poids=(unsigned char)f_Poids;
@@ -191,8 +192,10 @@ void Lecture_Poids(unsigned int *pu8_Poids,unsigned int *pu8_ID_CAN) {
     *pu8_Poids=0;
   }
   
+#ifdef DEBUG
   Serial.print("*pu8_Poids:");  
   Serial.println(*pu8_Poids, 1);
+#endif
 
   /* Si Demande de Reinit et Poids supérieur à 0 */
   if ((*pu8_Poids > 5)&&(b_Reinit != 0))
@@ -208,6 +211,18 @@ void Lecture_Poids(unsigned int *pu8_Poids,unsigned int *pu8_ID_CAN) {
 }
 
 
+/*************************************************************/
+/*                        setup                              */
+/*************************************************************/
+void setup() {
+  Serial.begin(38400);
+  Init_Poids();
+  Init_Can(); 
+} 
+
+/*************************************************************/
+/*                            loop                           */
+/*************************************************************/
 void loop() {
   unsigned int u8_Poids_Moyen;
   static unsigned int u8_ID_CAN;
